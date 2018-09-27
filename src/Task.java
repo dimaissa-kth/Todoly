df94f4794dc35b2a2f9d4b87a844720c976630f5
tmp-sda-1182 <dimaa@kth.se>
2018-09-27 10:16:52 +0200

    test adding source file

---
+++ src/Task.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+/**
+ * 
+ */
+
+/**
+ * @author Dima Alissa
+ *
+ */
+public class Task {
+	
+	private String name;
+	private String description;
+	private Date dueDate;
+	
+	
+	public Task (String name,String description) {
+		
+	}
+	
+
+	
+	
+}
